836a9fc4cf2b1b01a53c17c183b621e804948211
igmendes <gr.mendes1@gmail.com>
2019-12-01 21:27:59 -0300

    Versão inicial projeto - efetiva

---
+++ src/main/java/com/kazale/api/MeuPromeiroPojetoApplication.java
@@ -0,0 +1,13 @@
+package com.kazale.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPromeiroPojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPromeiroPojetoApplication.class, args);
+	}	
+
+}
